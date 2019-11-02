e09cff13aef23acfbbf38aadfd653930e137760f
InAudibleDayne <daynebechtold@hotmail.com>
2019-11-02 16:32:41 -0600

    Refactored FontAwesome Icon calls into a helper file.

---
+++ src/helpers/icons.js
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
+const Icons = () => {
+    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+};
+
+export default Icons;
