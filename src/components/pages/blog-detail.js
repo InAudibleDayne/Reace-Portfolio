c7ab89b205a7f9110a0562f401768805f38694c3
InAudibleDayne <daynebechtold@hotmail.com>
2019-11-05 13:41:50 -0700

    added link to edit mode in blog detail page for admin

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from '../blog/blog-featured-image';
 
 export default class BlogDetail extends Component {
@@ -12,7 +13,14 @@ export default class BlogDetail extends Component {
             currentId: this.props.match.params.slug,
             blogItem: {},
             editMode: false
-        }
+        };
+
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleEditClick() {
+        console.log("handle edit clicked");
+        this.setState({ editMode: true });
     }
 
     getBlogItem() {
@@ -38,18 +46,26 @@ export default class BlogDetail extends Component {
         blog_status
     } = this.state.blogItem;
 
-    return (
-        <div className="blog-container">
+    const contentManager = () => {
+        if (this.state.editMode) {
+            return <BlogForm />
+        } else {
+            return (
             <div className="content-container">
-                <h1>{title}</h1>
+                <h1 onClick={this.handleEditClick}>{title}</h1>
 
                 <BlogFeaturedImage img={featured_image_url} />
 
                 <div className="content">
                     {ReactHtmlParser(content)}
                 </div>
-            </div>
-        </div>
+            </div> 
+            )
+        }
+    }
+
+    return (
+        <div className="blog-container">{contentManager()}</div>
     );
   }
 }
