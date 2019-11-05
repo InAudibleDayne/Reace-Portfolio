bd7dbe4d86639f9490a8e30be5307ec0d2688878
InAudibleDayne <daynebechtold@hotmail.com>
2019-11-05 16:37:08 -0700

    Began work on about page and added styling for the columns

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import profilePicture from "../../../static/assets/images/Photo-of-me.jpg";
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div 
+            className="left-column"
+            style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+            <div className="right-column">
+                Bio goes here as well as skills and experiences
+            </div>
         </div>
     );
 }
